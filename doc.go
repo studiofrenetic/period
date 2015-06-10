f1722e4362e96efd0b0a2a23285626199dc4283d
Mathieu Doyon <mathieu.doyon@studiofrenetic.com>
2015-06-10 09:51:19 -0400

    Adding some docs

--- doc.go
+++ doc.go
@@ -16,15 +16,25 @@
 // Examples
 //
 //     // Accessing time range properties
-//     // [Code here]
-//
-//     // Iterate over a time range
-//     // [Code here]
+//     p, err := period.CreateFromMonth(2015, 3) // {2015-01-12 00:00:00 +0000 UTC 2015-01-19 00:00:00 +0000 UTC}
+//     if err != nil {
+//         log.Fatal(err)
+//     }
 //
 //     // Comparing time ranges
-//     // [Code here]
+//     p, err := period.CreateFromWeek(2015, 3)
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+//     alt := period.CreateFromDuration(time.Date(2015, 1, 14, 0, 0, 0, 0, time.UTC), time.Duration(24*7)*time.Hour) // {2015-01-14 00:00:00 +0000 UTC 2015-01-21 00:00:00 +0000 UTC}
+//     sameDuration := p.SameDurationAs(alt) // true
+//
+//     p := period.CreateFromDuration(time.Date(2015, 1, 1, 0, 0, 0, 0, time.UTC), (time.Duration(2) * time.Hour))
+//     shouldContains := time.Date(2015, 1, 1, 0, 30, 0, 0, time.UTC)
+//     contains := p.Contains(shouldContains)
+//
 //
 //     // Modifying time ranges
-//     // [Code here]
+//     p.Next()
 //
 package period
